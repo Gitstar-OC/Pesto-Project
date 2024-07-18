435625fb8cc2b57ab3ae629682f2859a2bd9aca8
OC <152989429+Gitstar-OC@users.noreply.github.com>
2024-07-18 20:27:07 +0530

    Modified vercel.json file and added requirements.txt file

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,7 +3,7 @@ import { HelmetProvider } from "react-helmet-async";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // Ensure this is correctly imported
+import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
